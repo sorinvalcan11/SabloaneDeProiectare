569f169749aa13944684f30e0fcac5322fda3096
sorinvalcan11 <sorin.valcan@gmail.com>
2017-10-31 10:04:45 +0200

    Added AbstractElement to implement Add Remove and GetElement with Throw Exception only once

---
+++ SabloaneDeProiectare/SabloaneDeProiectare/AbstractElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    abstract class AbstractElement : IElement
+    {
+        public void Add(IElement newElement)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public void Remove(IElement element)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public IElement GetElement(int index)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public abstract string Print();
+
+    }
+}
