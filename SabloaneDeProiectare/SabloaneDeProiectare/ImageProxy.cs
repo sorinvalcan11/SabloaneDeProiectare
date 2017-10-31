3674dbf4758fbb8ddf312317175858d4eca7e875
sorinvalcan11 <sorin.valcan@gmail.com>
2017-10-31 10:10:59 +0200

    Added ImageProxy

---
+++ SabloaneDeProiectare/SabloaneDeProiectare/ImageProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class ImageProxy : AbstractElement
+    {
+        private string fileName;
+        private Imagine realImage = null;
+
+        public ImageProxy(string fName)
+        {
+            fileName = fName;
+        }
+
+        public override string Print()
+        {
+            realImage = new Imagine(fileName);
+
+            return realImage.Print();
+        }
+    }
+}
