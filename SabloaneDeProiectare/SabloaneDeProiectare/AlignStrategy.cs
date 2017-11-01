f0d4ed81929c56b4574837eb1b6bc688d05e0d71
sorinvalcan11 <sorin.valcan@gmail.com>
2017-11-01 13:30:17 +0200

    Added AlignStrategy and 3 derived classes

---
+++ SabloaneDeProiectare/SabloaneDeProiectare/AlignStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabloaneDeProiectare
+{
+    abstract class AlignStrategy
+    {
+        protected string text;
+
+        public AlignStrategy(string text)
+        {
+            this.text = text;
+        }
+
+        public abstract string PrintAligned();
+    }
+}
